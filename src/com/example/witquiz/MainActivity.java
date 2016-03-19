f83f90faa75cd3a3c76eceabef9b3cbd09eef751
Aleksandra Majchrzak <briza.6@gmail.com>
2016-03-19 16:28:38 +0100

    Menu activity pt 2 (layout)

--- src/com/example/witquiz/MainActivity.java
+++ src/com/example/witquiz/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.witquiz;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	public static final String SHARED_PREFERENCES = "WIT_PREFERENCES";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        loadControls();
+    }
+    
+    public void loadControls() {
+    	
+    	Button startButton = (Button) findViewById(R.id.start_button);
+    	
+    	startButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				
+				EditText usernameExitText = (EditText) findViewById(R.id.username_editText);
+				String userName = usernameExitText.getText().toString();
+				
+				SharedPreferences shared = getSharedPreferences(SHARED_PREFERENCES, 0);
+				shared.edit()
+						.putString("CurrentUser", userName)
+						.commit();
+				
+				Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+				startActivity(intent);
+			}
+    		
+    	});
     }
 }
--- src/com/example/witquiz/MenuActivity.java
+++ src/com/example/witquiz/MenuActivity.java
@@ -2,8 +2,6 @@ package com.example.witquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MenuActivity extends Activity {
 
